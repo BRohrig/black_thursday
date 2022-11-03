40ab442f90f9f573e835f4fbbd96f696bbd489d4
Alastair Lloyd <dex8056@aol.com>
2022-11-03 07:40:43 -0600

    feat: Amend argument names

--- lib/invoiceitem.rb
+++ lib/invoiceitem.rb
@@ -7,14 +7,14 @@ class InvoiceItem
               :created_at,
               :updated_at
 
-  def initialize(ii_input)
-    @id           = ii_input[:id]
-    @item_id      = ii_input[:item_id]
-    @invoice_id   = ii_input[:invoice_id]
-    @quantity     = ii_input[:quantity]
-    @unit_price   = ii_input[:unit_price]
-    @created_at   = ii_input[:created_at]
-    @updated_at   = ii_input[:updated_at]
+  def initialize(attributes)
+    @id           = attributes[:id]
+    @item_id      = attributes[:item_id]
+    @invoice_id   = attributes[:invoice_id]
+    @quantity     = attributes[:quantity]
+    @unit_price   = attributes[:unit_price]
+    @created_at   = attributes[:created_at]
+    @updated_at   = attributes[:updated_at]
   end
 
   def unit_price_to_dollars
