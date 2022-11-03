032ad611549c71236328705f335d0d696357ba00
Alastair Lloyd <dex8056@aol.com>
2022-11-03 10:25:03 -0600

    feat/test: Add and pass exits test

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,10 +2,10 @@ require './lib/transaction'
 
 RSpec.describe Transaction do
   let(:transaction) {Transaction.new({
-    :id => 1, 
-    :invoice_id => 2,
-    :credit_card_number => 3,
-    :credit_card_expiration_date  => 4,
+    :id => 6, 
+    :invoice_id => 8,
+    :credit_card_number => "4242424242424242",
+    :credit_card_expiration_date  => "0220",
     :result => "success",
     :created_at => Time.now,
     :updated_at => Time.now
@@ -15,10 +15,10 @@ RSpec.describe Transaction do
     allow(Time).to receive(:now).and_return(@time_now)
 
     expect(transaction).to be_a(Transaction)
-    expect(transaction.id).to eq(1)
-    expect(transaction.invoice_id).to eq(2)
-    expect(transaction.credit_card_number).to eq(3)
-    expect(transaction.credit_card_expiration_date).to eq(4)
+    expect(transaction.id).to eq(6)
+    expect(transaction.invoice_id).to eq(8)
+    expect(transaction.credit_card_number).to eq("4242424242424242")
+    expect(transaction.credit_card_expiration_date).to eq("0220")
     expect(transaction.result).to eq("success")
     expect(transaction.created_at).to eq(Time.now)
     expect(transaction.updated_at).to eq(Time.now)
